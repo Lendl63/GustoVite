f508eec21e9905d462f93eeea15897ee36aba35e
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-01-09 19:09:52 +0100

    feat: Ajout du menu burger pour responsive
    Stylisation et navigation du menu burger (html/css/js) terminer)

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-//==================================
+//=============================================================
 // 1. NAVIGATION ENTRE LES PAGES
-//==================================
+//=============================================================
 
 //  index.html
-const navBtn = document.querySelectorAll(".more-btn");
+const linkBtn = document.querySelectorAll(".more-btn");
 
-navBtn.forEach((btn) => {
+linkBtn.forEach((btn) => {
     btn.addEventListener('click', function() {
         // recuperation de href de la page
         const hrefPage = this.dataset.href;
@@ -14,5 +14,46 @@ navBtn.forEach((btn) => {
             window.location.href = hrefPage;
         }
     });
+});
+
+//=============================================================
+// 2. NAVIGATION AVEC MENU MODAL
+//=============================================================
+
+const navBtn = document.getElementById("navBtn");
+const menuModal = document.getElementById("menuModal");
+const closeBtn = document.getElementById("closeBtn");
+
+const openModal = () => {
+    menuModal.classList.add("active");
+    document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+    menuModal.classList.remove("active");
+    document.body.style.overflow = "";
+};
+
+navBtn?.addEventListener('click', openModal);
+closeBtn?.addEventListener('click', closeModal);
+
+window.addEventListener('click', (event) => {
+    // Ferme modal nav si click extérieur
+    if (event.target === menuModal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A' && menuModal.classList.contains('active')) {
+        closeModal();
+    }
+});
+
+// Fermer avec Escape (plus standard que Espace)
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        if (menuModal.classList.contains("active")) closeModal();
+    }
 });
 
